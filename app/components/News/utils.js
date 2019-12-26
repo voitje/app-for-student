cecaf2c29beaca1bf97d50670eebbf8a29c58078
shagaevia <shagaevia@sib-soft.ru>
2019-12-26 12:57:53 +0700

    test

--- app/components/News/utils.js
+++ app/components/News/utils.js
@@ -3,7 +3,8 @@ import { API_KEY } from '../../config/env';
 const url = `https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-07&sortBy=publishedAt&apiKey=${API_KEY}`;
 
 export async function getUSANews() {
-    let result = await fetch('https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-09&sortBy=publishedAt&apiKey=7467b39f033842e5b17ea55858b2942c')
+    let result = await fetch(
+        'https://newsapi.org/v2/everything?q=bitcoin&from=2019-10-09&sortBy=publishedAt&apiKey=7467b39f033842e5b17ea55858b2942c')
             .then(response => response.json());
     return result.articles;
 }
